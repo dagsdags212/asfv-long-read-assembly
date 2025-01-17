358c8139f502e83134ecf50c94312eadc25ff7e2
dagsdags212 <jegsamson.dev@gmail.com>
2025-01-18 00:42:38 +0800

    replaced relative filepaths to absolute filepaths

--- scripts/globals.sh
+++ scripts/globals.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Absolute path of project
+ROOT=$(dirname $(dirname $(realpath "$0")))
+
 # Sequencing project accession
 PRJNA=PRJNA857442
 
@@ -7,4 +10,10 @@ PRJNA=PRJNA857442
 ASM=ON963982.1
 
 # Downloaded genome filepath
-REF=data/ref.fna
+SWINE_REF=${ROOT}/data/genomes/swine_ref.fna
+
+# Path to raw reads
+RAW_READS=${ROOT}/data/reads/raw
+
+# Path to trimmed reads
+TRIMMED_READS=${ROOT}/data/reads/trimmed
