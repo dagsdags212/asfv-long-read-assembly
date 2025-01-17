72909e5c7b34e9e4c2ae4798ce31081eb9dcef6f
dagsdags212 <jegsamson.dev@gmail.com>
2025-01-18 00:41:56 +0800

    updated path of filtered reads to be used for assembly

--- scripts/04_assemble.sh
+++ scripts/04_assemble.sh
@@ -2,7 +2,7 @@
 
 # Perform de novo assembly with flye
 run_flye_assembler() {
+  local reads=output/samtools/filtered_reads.fastq
   mkdir -p output/flye
-  flye --nano-hq output/unmapped.complete.fastq.gz \
-    --genome-size 200000 --meta -o output/flye
+  flye --nano-hq ${reads} --genome-size 200000 --meta -o output/flye
 }
