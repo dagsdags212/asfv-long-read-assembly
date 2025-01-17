0a71c29111c549d5b68619902d1a89d6aed4e92f
dagsdags212 <jegsamson.dev@gmail.com>
2025-01-17 22:08:07 +0800

    updated imports to use absolute paths

--- scripts/00_run_complete_pipeline.sh
+++ scripts/00_run_complete_pipeline.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env bash
 
+ROOT=$(dirname $(realpath "$0"))
+
 # print commands and exit on error
 set -eu
 
 # Load variables
-source scripts/globals.sh
+source ${ROOT}/scripts/globals.sh
 
 # Retrieve read and reference data
-source scripts/01_download_data.sh
+source ${ROOT}/scripts/01_download_data.sh
 
 # Perform adapter trimming
-source scripts/02_trim_reads.sh
+source ${ROOT}/scripts/02_trim_reads.sh
 
 # Map reads to reference and remove host contaminants
-source scripts/03_map_and_filter.sh
+source ${ROOT}/scripts/03_map_and_filter.sh
 
 # Run de novo assembler
-source scripts/04_assemble.sh
+source ${ROOT}/scripts/04_assemble.sh
 
 # Run the entire pipeline
 run_pipeline() {
   download_data
-  run_porechop
+  trim_adapters
   index_ref
   run_minimap
   sam2bam
