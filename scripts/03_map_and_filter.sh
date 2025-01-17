44a010cec7732f4ad5211917f4cd0e2e57801123
dagsdags212 <jegsamson.dev@gmail.com>
2025-01-18 00:41:12 +0800

    functions now operate on a single FASTQ file; removed unneccesary sorting and BAM conversions

--- scripts/03_map_and_filter.sh
+++ scripts/03_map_and_filter.sh
@@ -1,38 +1,34 @@
 #!/usr/bin/env bash
 
 # Generate a samtools index for the reference file
-index_ref() {
-  echo "Indexing reference file: ${REF}"
-  samtools faidx ${REF}
+index_swine_ref() {
+  echo "Indexing reference file: ${SWINE_REF}"
+  samtools faidx ${SWINE_REF}
 }
 
 # Map reads against the reference genome
 run_minimap() {
+  echo "Aligning reads to the swine reference genome"
   mkdir -p output/minimap
-  parallel -j 2 -a data/accessions.txt \
-    minimap2 -a -x map-ont -o output/minimap/{}.sam ${REF} ${TRIMMED_READS}/{}.trimmed.fastq
-}
-
-# Convert SAM to BAM
-sam2bam() {
-  mkdir -p output/samtools
-  parallel -j 4 -a data/accessions.txt \
-    samtools view -b -h -o output/samtools/{}.bam output/minimap/{}.sam
-}
-
-# Concatenate all BAM files into a single master BAM file
-concat_bam_files() {
-  local target=output/complete.bam
-  samtools merge ${target} output/samtools/*.bam
+  minimap2 -a -x map-ont ${SWINE_REF} ${TRIMMED_READS}/all_reads.trimmed.fastq.gz >output/minimap/aln.sam
 }
 
 # Extract unmapped reads and store into a separate BAM file
-filter_aligned_reads() {
-  samtools -b -f 4 output/complete.bam >output/unmapped.complete.bam
+filter_unampped_reads() {
+  echo "Extracting unmapped reads"
+  samtools view -S -f 4 output/minimap/aln.sam >output/samtools/aln.unmapped.sam
 }
 
-# Convet unmapped BAM file to FASTQ format
-bam2fastq() {
-  local target=output/unmapped.complete.fastq.gz
-  samtools bam2fq output/unmapped.complete.bam >${target}
+# Convert unmapped BAM file to FASTQ format
+sam2fastq() {
+  echo "Converting BAM to FASTQ"
+  local target=output/samtools/aln.unmapped.sam
+  samtools fastq ${target} >output/samtools/filtered_reads.fastq
+}
+
+map_and_filter() {
+  #index_swine_ref
+  run_minimap
+  filter_unampped_reads
+  sam2fastq
 }
