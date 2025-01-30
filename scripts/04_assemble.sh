2d455d8b682a4afdb4cfa8d14202d25c9195f677
dagsdags212 <jegsamson.dev@gmail.com>
2025-01-30 20:29:11 +0800

    added --iterations flag for flye assembly

--- scripts/04_assemble.sh
+++ scripts/04_assemble.sh
@@ -8,5 +8,5 @@ run_flye_assembler() {
   echo "[${FILENAME}] Performing de novo assembly with flye"
   local reads=output/samtools/filtered_reads.fastq
   mkdir -p output/flye
-  flye --nano-hq ${reads} --genome-size 200000 --meta -o output/flye
+  flye --nano-hq ${reads} --genome-size 0.2m --meta --threads 12 --iterations 5 -o output/flye
 }
